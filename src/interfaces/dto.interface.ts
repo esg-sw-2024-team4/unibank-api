0a02ca3fe1014f76079bf22671abc0b4f5994324
Konerd <adr10won@gmail.com>
2024-08-28 13:26:13 +0900

    chore: 응답 데이터에 대한 공통 인터페이스 정의 및 적용

---
+++ src/interfaces/dto.interface.ts
@@ -0,0 +1,12 @@
+export interface IMetadata {
+    total: number;
+}
+
+export interface ISResponse<T> {
+    metadata: IMetadata;
+    data: T[];
+}
+
+export interface IMResponse<T> {
+    data: T;
+}
