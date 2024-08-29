cd3196a4e423fd08459de96e8af4af3a639f94ac
hyj0327 <hyj_0327@naver.com>
2024-08-29 15:06:08 +0900

    feat: Question 및 Subject 모델 추가
    - Question 및 Subject 모델 설계 및 초기 설정
    - Question 모델과 Subject, User 모델 간의 관계 설정

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,24 @@
 import User from './user.model';
+import Question from './question.model';
+import Subject from './subject.model';
+
+// 관계 설정
+Question.belongsTo(Subject, {
+  foreignKey: 'subjectId',
+  targetKey: 'id',
+});
+Question.belongsTo(User, {
+  foreignKey: 'authorId',
+  targetKey: 'id',
+});
+
+Subject.hasMany(Question, { foreignKey: 'subjectId', sourceKey: 'id' });
+User.hasMany(Question, { foreignKey: 'authorId', sourceKey: 'id' });
 
 const models = {
   User,
+  Question,
+  Subject,
 };
 
 export default models;
