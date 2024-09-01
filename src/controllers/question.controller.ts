016a6669c15125be353729d12e9b1bb7aa76a76d
hyj0327 <hyj_0327@naver.com>
2024-09-01 23:30:56 +0900

    feat: 문제 관련 controller,service, routes 추가
    - question controller,routes,service 추가
    - question.model: id INTEGER.UNSIGNED에서 INTEGER로 변경
    - dto.interface: Question 관련 requestBody 받을 IQuestionData 추가
    - question.service 관련 테스트 코드 작성

--- src/controllers/question.controller.ts
+++ src/controllers/question.controller.ts
@@ -1,7 +1,54 @@
+import { AuthRequest } from '../interfaces/http.interface';
+import { Response } from 'express';
 import { asyncHandler } from '../utils/async-handler.util';
 
-import { findQuestions } from '../services/question.service';
+import {
+  addQuestion,
+  findQuestionById,
+  findQuestions,
+  modifyQuestion,
+  removeQuestion,
+} from '../services/question.service';
+import { withTransaction } from '../utils/with-transaction.util';
 
-export const getQuestions = asyncHandler(async (req, res) => {
-  res.json(await findQuestions());
-});
+export const getQuestions = asyncHandler(
+  async (req: AuthRequest, res: Response) => {
+    res.json(await findQuestions());
+  },
+);
+
+export const getQuestionById = asyncHandler(
+  async (req: AuthRequest, res: Response) => {
+    const { id } = req.params;
+    res.json(await findQuestionById(Number(id)));
+  },
+);
+
+export const createQuestion = asyncHandler(
+  async (req: AuthRequest, res: Response) => {
+    const questionData = req.body;
+
+    res.json(
+      await withTransaction(async (tx) => addQuestion(questionData, tx)),
+    );
+  },
+);
+
+export const updateQuestion = asyncHandler(
+  async (req: AuthRequest, res: Response) => {
+    const { id } = req.params;
+    const questionData = req.body;
+    await withTransaction(async (tx) =>
+      modifyQuestion(Number(id), questionData, tx),
+    );
+    res.status(204).end();
+  },
+);
+
+export const deleteQuestion = asyncHandler(
+  async (req: AuthRequest, res: Response) => {
+    const { id } = req.params;
+    await withTransaction(async (tx) => removeQuestion(Number(id), tx));
+    res.status(204).end();
+  },
+);
--- src/services/question.service.ts
+++ src/services/question.service.ts
@@ -1,5 +1,72 @@
-import { IMResponse } from '../interfaces/dto.interface';
+import { Transaction } from 'sequelize';
+import { IQuestionData, ISResponse } from '../interfaces/dto.interface';
+import Question from '../models/question.model';
+
+const toCamelCase = (snakeStr: string): string => {
+  return snakeStr.replace(/(_\w)/g, (match) => match[1].toUpperCase());
+};
+const convertKeysToCamel = (obj: any): any => {
+  if (Array.isArray(obj)) {
+    return obj.map((item) => convertKeysToCamel(item));
+  } else if (obj !== null && obj.constructor === Object) {
+    return Object.keys(obj).reduce((acc, key) => {
+      acc[toCamelCase(key)] = convertKeysToCamel(obj[key]);
+      return acc;
+    }, {} as any);
+  }
+  return obj;
+};
 
 export const findQuestions = async () => {
-  return {} as IMResponse<any>;
+  const { count, rows } = await Question.findAndCountAll({
+    attributes: {
+      exclude: ['correct_answer', 'explanation', 'created_at', 'updated_at'],
+    },
+  });
+  return {
+    metadata: {
+      total: count,
+    },
+    data: rows,
+  } as ISResponse<any>;
+};
+
+export const findQuestionById = async (id: number) => {
+  const question = await Question.findByPk(id);
+  if (!question) {
+    throw new Error('');
+  }
+  return question;
+};
+
+export const addQuestion = async (
+  questionData: IQuestionData,
+  transaction: Transaction,
+) => {
+  const data = convertKeysToCamel(questionData);
+
+  const newQuestion = await Question.create(data, { transaction });
+  return newQuestion;
+};
+
+export const modifyQuestion = async (
+  id: number,
+  updateData: Partial<IQuestionData>,
+  transaction: Transaction,
+) => {
+  const question = await Question.findByPk(id);
+  if (!question) {
+    throw new Error('');
+  }
+  const data = convertKeysToCamel(updateData);
+  await question.update(data, { transaction });
+};
+
+export const removeQuestion = async (id: number, transaction: Transaction) => {
+  const question = await Question.findByPk(id);
+
+  if (!question) {
+    throw new Error('');
+  }
+  await question.destroy({ transaction });
 };
