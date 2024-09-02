9b7edc65a90942f89845b90a77c8561e9a70a134
Konerd <adr10won@gmail.com>
2024-09-02 18:48:36 +0900

    refactor: 빌드 시 오류가 발생할 수 있는 코드 수정 및 일부 생략 가능한 코드 제거

--- src/controllers/question.controller.ts
+++ src/controllers/question.controller.ts
@@ -1,7 +1,4 @@
-import { AuthRequest } from '../interfaces/http.interface';
-import { Response } from 'express';
 import { asyncHandler } from '../utils/async-handler.util';
-
 import {
   addQuestion,
   findQuestionById,
@@ -11,44 +8,31 @@ import {
 } from '../services/question.service';
 import { withTransaction } from '../utils/with-transaction.util';
 
-export const getQuestions = asyncHandler(
-  async (req: AuthRequest, res: Response) => {
-    res.json(await findQuestions());
-  },
-);
+export const getQuestions = asyncHandler(async (_, res) => {
+  res.json(await findQuestions());
+});
 
-export const getQuestionById = asyncHandler(
-  async (req: AuthRequest, res: Response) => {
-    const { id } = req.params;
-    res.json(await findQuestionById(Number(id)));
-  },
-);
+export const getQuestionById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  res.json(await findQuestionById(Number(id)));
+});
 
-export const createQuestion = asyncHandler(
-  async (req: AuthRequest, res: Response) => {
-    const questionData = { ...req.body, author_id: req.user!.id };
+export const createQuestion = asyncHandler(async (req, res) => {
+  const questionData = { ...req.body, author_id: req.user!.id };
+  res.json(await withTransaction(async (tx) => addQuestion(questionData, tx)));
+});
 
-    res.json(
-      await withTransaction(async (tx) => addQuestion(questionData, tx)),
-    );
-  },
-);
+export const updateQuestion = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const questionData = req.body;
+  await withTransaction(async (tx) =>
+    modifyQuestion(Number(id), questionData, tx),
+  );
+  res.status(204).end();
+});
 
-export const updateQuestion = asyncHandler(
-  async (req: AuthRequest, res: Response) => {
-    const { id } = req.params;
-    const questionData = req.body;
-    await withTransaction(async (tx) =>
-      modifyQuestion(Number(id), questionData, tx),
-    );
-    res.status(204).end();
-  },
-);
-
-export const deleteQuestion = asyncHandler(
-  async (req: AuthRequest, res: Response) => {
-    const { id } = req.params;
-    await withTransaction(async (tx) => removeQuestion(Number(id), tx));
-    res.status(204).end();
-  },
-);
+export const deleteQuestion = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  await withTransaction(async (tx) => removeQuestion(Number(id), tx));
+  res.status(204).end();
+});
